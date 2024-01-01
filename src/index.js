6cf3ca7cc47655b5cb2687e86e9a2bda54b82119
Henrique Heron <henriqueheron1@gmail.com>
2024-01-01 17:06:02 -0300

    feat: latest added task can now be stored within the project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import { Task, showTaskForm, hideTaskForm } from './taskHandler.js';
+import { Task, displayTask, showTaskForm, hideTaskForm } from './taskHandler.js';
+import { taskList } from './taskHandler.js';
 import { createProject, getProjectTasks, displayProjects, deleteProject } from './projectHandler.js';
 import './styles.css';
 
 document.addEventListener("DOMContentLoaded", () => {
+    let taskCount = 1;
+    let currentProject;
 
     const myProjects = [];
     if (localStorage.getItem('projectTitles')) {
         myProjects.push(...JSON.parse(localStorage.getItem('projectTitles')));
+        // clears for eventual null projects
+        myProjects.forEach(project => {
+            if (!project) {
+                myProjects.splice(myProjects.indexOf(project), 1);
+            }
+        })
+
         console.log('Starting myProjects array:');
         console.log(myProjects);
         for (const title of myProjects) {
@@ -15,11 +25,25 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     refreshProjectEventListeners();
 
+    if (localStorage.tasks) {
+        taskList.push(JSON.parse(localStorage.getItem('tasks')))
+        taskList.forEach(task => {
+            console.log('This is the task:');
+            console.log(task);
+            task.forEach(obj => {
+                console.log('this is the project:');
+                console.log(obj);
+                const madeTask = displayTask(obj, taskCount, obj.project)
+                console.log(madeTask);
+            })
+        })
+    }
+
     const createProjectBtn = document.querySelector('#create-project-button');
     createProjectBtn.addEventListener('click', () => {
 
         let projectTitle = prompt('Type in your project name');
-        if (projectTitle){
+        if (projectTitle) {
             createProject(projectTitle);
             localStorage.setItem('projectTitles', JSON.stringify(myProjects));
         } else {
@@ -31,19 +55,20 @@ document.addEventListener("DOMContentLoaded", () => {
         refreshProjectEventListeners();
     })
 
-    let currentProject;
-
     function refreshProjectEventListeners() {
         const projects = document.querySelectorAll('.project');
         projects.forEach(project => {
             project.addEventListener('click', () => {
                 currentProject = project;
-                // console.log();
                 getProjectTasks(currentProject.id);
             })
         })
     }
 
+    function returnToProjects() {
+        hideTaskForm();
+        displayProjects();
+    }
 
     const addTaskBtn = document.querySelector('#add-task-button')
     const createTaskBtn = document.querySelector('#task-creation-button');
@@ -53,27 +78,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const returnToProjectsBtn = document.querySelector('#projects-button');
     const deleteProjectBtn = document.querySelector('#delete-project-button');
-    
-    let taskCount = 1;
+
 
     addTaskBtn.addEventListener('click', () => {
         showTaskForm();
     })
 
-    createTaskBtn.addEventListener('click', (e) => {
-        Task(taskTitle.value, taskDueDate.value, taskCount, currentProject);
+    createTaskBtn.addEventListener('click', () => {
+        const builtTask = Task(taskTitle.value, taskDueDate.value, taskCount, currentProject.id);
+        taskList.push(builtTask);
+        localStorage.setItem('tasks', JSON.stringify(taskList));
+        console.log('Built task:');
+        console.log(builtTask);
+        console.log('Task list:');
+        console.log(taskList);
         taskCount++;
         taskTitle.value === '';
         taskDueDate.value === '';
     })
 
     returnToProjectsBtn.addEventListener('click', () => {
-        hideTaskForm();
-        displayProjects();
+        returnToProjects();
     })
 
     deleteProjectBtn.addEventListener('click', () => {
         deleteProject(currentProject, myProjects);
         refreshProjectEventListeners();
+        returnToProjects();
     })
 })
--- src/taskHandler.js
+++ src/taskHandler.js
@@ -1,36 +1,32 @@
 const tasksContainer = document.querySelector('#tasks-container');
 
 // const taskPriority = document.querySelector('#task-priority');
-const taskList = [];
+export const taskList = [];
 
 export function Task(title, dueDate, taskCount, parentProject) {
+    console.log(title);
     const newTask = {};
     newTask.title = title;
     newTask.dueDate = dueDate; 
     newTask.priority = (function() {
         const priorityElement = document.querySelector('#priority');
-        console.log(`Priority of task '${newTask.title}' is ${priorityElement.value}.`);
         return priorityElement.value;
     })();
     newTask.project = parentProject;
 
     displayTask(newTask, taskCount, parentProject);
-    taskList.push(newTask);
     return newTask;
 }
 
-
-function displayTask(task, count, parentProject) {
+export function displayTask(task, count, parentProject) {
 
     const div = document.createElement('div');
     div.classList.add('task', 'mt-1', 'bg-secondary', 'rounded', 'p-2', 'ps-3');
     div.id = `task${count}`;
-    console.log(div.id);
     
     const rowDiv = document.createElement('div');
     rowDiv.classList.add('row');
     div.appendChild(rowDiv);
-    
     
     const colDiv1 = document.createElement('div');
     colDiv1.classList.add('col-sm');
@@ -53,21 +49,15 @@ function displayTask(task, count, parentProject) {
     img.classList.add('delete-button');
     img.src = '../src/icons/delete-icon.svg';
     img.title = 'Delete Task';
-
-    img.innerHTML = `<title>Delete Task</title>
-    <path
-        d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />`;
-
     img.addEventListener('click', () => {
-        div.remove();
-        console.log(`Successfully removed ${div.id}.`);
+        deleteTask(div);
     })
     colDiv2.appendChild(img);
 
     div.setAttribute('data-childofproject', parentProject)
-    console.log('Parent project of the task is ' + parentProject);
 
     tasksContainer.appendChild(div);
+    return div;
 }
 
 export function showTaskForm() {
@@ -86,4 +76,9 @@ export function hideTaskForm() {
         TaskCreationContainer.classList.add('d-none');
         console.log('Hid form');
     }
+}
+
+export function deleteTask(selectedTask) {
+    selectedTask.remove();
+    console.log(`Successfully removed ${selectedTask.id}.`);
 }
